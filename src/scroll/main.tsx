be445809f23bbce47605195ce76afe6ac3760848
myuon <ioi.joi.koi.loi@gmail.com>
2024-03-18 02:45:14 +0900

    feat: scroll demo

--- src/scroll/main.tsx
+++ src/scroll/main.tsx
@@ -13,6 +13,92 @@ const main = () => {
   });
   app.stage.eventMode = "static";
   app.stage.hitArea = app.screen;
+  app.stage.sortableChildren = true;
+
+  interface Resource {
+    x: number;
+    y: number;
+    color: number;
+    dom: PIXI.Graphics;
+  }
+
+  const model: Resource[] = [];
+  const add = (r: Partial<Resource>) => {
+    const dom = new PIXI.Graphics();
+    dom.beginFill(r.color || 0xff0000);
+    dom.drawRect(0, 0, 50, 50);
+    dom.endFill();
+    dom.position.set(r.x || 0, r.y || 0);
+    dom.zIndex = 1;
+
+    app.stage.addChild(dom);
+    model.push({ x: r.x || 0, y: r.y || 0, dom, color: r.color || 0xff0000 });
+  };
+  const reposition = () => {
+    for (const r of model) {
+      r.dom.position.set(r.x - mapPoint.x, r.y - mapPoint.y);
+    }
+  };
+
+  const mapSize = { width: 3500, height: 750 };
+  const mapPoint = {
+    x: 0,
+    y: 0,
+    dom: new PIXI.Text("", {
+      fontFamily: "sans-serif",
+      fill: 0xffffff,
+    }),
+  };
+  mapPoint.dom.zIndex = 2;
+  app.stage.addChild(mapPoint.dom);
+
+  const scrollBarX = new PIXI.Graphics();
+  scrollBarX.beginFill(0xff0000);
+  scrollBarX.drawRect(
+    0,
+    0,
+    (canvasSize.width * canvasSize.width) / mapSize.width,
+    30,
+  );
+  scrollBarX.endFill();
+  scrollBarX.position.set(0, canvasSize.height - 10);
+  app.stage.addChild(scrollBarX);
+
+  const scrollBarY = new PIXI.Graphics();
+  scrollBarY.beginFill(0xff0000);
+  scrollBarY.drawRect(
+    0,
+    0,
+    30,
+    (canvasSize.height * canvasSize.height) / mapSize.height,
+  );
+  scrollBarY.endFill();
+  scrollBarY.position.set(canvasSize.width - 10, 0);
+  app.stage.addChild(scrollBarY);
+
+  const updateMapPoint = (x: number, y: number) => {
+    mapPoint.x = Math.max(0, Math.min(mapSize.width - canvasSize.width, x));
+    mapPoint.y = Math.max(0, Math.min(mapSize.height - canvasSize.height, y));
+    mapPoint.dom.text = `X: ${x}, Y: ${y}`;
+
+    scrollBarX.position.x = (mapPoint.x * canvasSize.width) / mapSize.width;
+    scrollBarY.position.y = (mapPoint.y * canvasSize.height) / mapSize.height;
+
+    reposition();
+  };
+  updateMapPoint(0, 0);
+
+  for (let i = 0; i < 50; i++) {
+    add({
+      x: Math.random() * mapSize.width,
+      y: Math.random() * mapSize.height,
+      color: Math.random() * 0xffffff,
+    });
+  }
+
+  app.stage.on("wheel", (event) => {
+    updateMapPoint(mapPoint.x + event.deltaX, mapPoint.y + event.deltaY);
+  });
 
   app.ticker.add(() => {
     sss.update();
